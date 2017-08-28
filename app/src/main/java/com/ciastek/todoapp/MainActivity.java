eb00ded8193c6ed724d3f61e4e78939a7af2568a
emcia <emciastek@gmail.com>
2017-08-28 10:15:32 +0200

    Navigation view added

--- app/src/main/java/com/ciastek/todoapp/MainActivity.java
+++ app/src/main/java/com/ciastek/todoapp/MainActivity.java
@@ -3,14 +3,21 @@ package com.ciastek.todoapp;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 
@@ -18,7 +25,7 @@ import com.ciastek.todoapp.database.DatabaseDescription;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, NavigationView.OnNavigationItemSelectedListener {
 
     public static final String TASK_URI = "task_uri";
     private static final int TASKS_LOADER = 0;
@@ -51,6 +58,15 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 startActivity(newTaskIntent);
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         getSupportLoaderManager().initLoader(TASKS_LOADER, null, this);
     }
@@ -88,4 +104,39 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected (@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.show_all) {
+            // Handle the camera action
+        } else if (id == R.id.show_done) {
+
+        } else if (id == R.id.show_open) {
+            Log.d("MainActivity", "show open");
+
+        } else if (id == R.id.show_today) {
+
+        } else if (id == R.id.sort_date) {
+            Log.d("MainActivity", "sort by date");
+
+
+        } else if (id == R.id.sort_priority) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed () {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
